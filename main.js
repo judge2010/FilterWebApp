483801c131c72c63a99702fa7b7853b10f81c863
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-03-09 14:51:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
- function preload(){
- 
- };
- function setup(){
-  canvas= createCanvas(250,250);
-  canvas.center();
+nose_X=0;
+nose_Y=0;
+function preload(){
+ clown_nose = loadImage("https://i.postimg.cc/yNnYqD8P/mustache.png");
+};
+function setup(){
+ canvas= createCanvas(350,350);
+ canvas.center();
+ video = createCapture(VIDEO);
+ video.hide();
+ poseNet=ml5.poseNet(video,modelLoaded);
+ poseNet.on('pose',gotPoses);
+};
 
- };
 
-function draw(){
-
-} 
+function save_photo(){
+save("photo.png")
+};
+function modelLoaded(){
+console.log("Model is loaded");
+};
+ function draw(){
+   
+   };
